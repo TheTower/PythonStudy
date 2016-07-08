b85951f13c0f9fbf2e98a330431db35b7d80abee
TheTower <16thetower@gmail.com>
2016-07-08 22:20:29 +0900

    Python Study

---
+++ HelloWorld.py
@@ -0,0 +1,28 @@
+import random
+import sys
+import os
+
+print("Hello Wolrd")
+
+#Comment
+
+'''
+Multiline string
+'''
+
+name = "Eunmi" # name = 'Eunmi'
+
+print(name)
+
+# 연산자 + - *
+print("5 ** 2 =", 5**2) #5*5
+print("5 / 2 =", 5/2)
+print("5 % 2 =", 5%2)
+print("5 // 2 =", 5//2)
+
+#함수 사용하기
+
+def sum(a, b):
+    return a+b
+
+print (sum(3,4))
